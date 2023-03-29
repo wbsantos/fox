f389b4f5e20a7b6d7f7975d2158d42f48a8127e6
William Barbosa <wbs.developer@gmail.com>
2023-03-29 08:46:26 -0300

    Creating data structure for database

---
+++ database/db-setup.sql
@@ -0,0 +1,102 @@
+--create database fox
+
+create table if not exists Holder --it's either an user or a group id
+(
+	id uuid default gen_random_uuid() not null,
+
+	CONSTRAINT PK_HOLDER PRIMARY KEY(id)
+)
+
+create table if not exists Group
+(
+	id uuid default gen_random_uuid() not null,
+	name varchar(255) not null,
+
+	CONSTRAINT PK_GROUP PRIMARY KEY(id),
+	CONSTRAINT FK_GROUP_HOLDER FOREIGN KEY (id) REFERENCES Holder(id),
+)
+
+create table if not exists User
+(
+	id uuid default gen_random_uuid() not null,
+	email varchar(255) not null,
+	login varchar(64) not null,
+	password bytea not null,
+	salt bytea not null,
+	hash_method smallint not null,
+	name varchar(255) not null,
+
+	CONSTRAINT PK_USER PRIMARY KEY(id),
+	CONSTRAINT FK_USER_HOLDER FOREIGN KEY (id) REFERENCES Holder(id),
+)
+
+create table if not exists UserGroup
+(
+	id int GENERATED ALWAYS AS IDENTITY not null,
+	userId uuid not null,
+	groupId uuid not null,
+
+	CONSTRAINT PK_USERGROUP PRIMARY KEY(id),
+	CONSTRAINT FK_USERGROUP_USER FOREIGN KEY (userId) REFERENCES User(id),
+	CONSTRAINT FK_USERGROUP_GROUP FOREIGN KEY (groupId) REFERENCES Group(id)
+)
+
+create table if not exists SystemPermission
+(
+	id int GENERATED ALWAYS AS IDENTITY not null,
+	holderId uuid not null,
+	permission varchar(255),
+
+	CONSTRAINT PK_SYSTEMPERMISSION PRIMARY KEY(id),
+	CONSTRAINT FK_USERGROUP_HOLDER FOREIGN KEY (holderId) REFERENCES Holder(id)
+)
+
+create table if not exists Document
+(
+	id uuid default gen_random_uuid() not null,
+	documentBinary bytea not null,
+	postedDate timestamptz
+)
+
+create table if not exists DocumentMetadata
+(
+	id int GENERATED ALWAYS AS IDENTITY not null,
+	documentId uuid not null,
+	key varchar(255),
+	value varchar(1024),
+
+	CONSTRAINT PK_DOCUMENTMETADA PRIMARY KEY(id),
+	CONSTRAINT FK_DOCUMENTMETADATA_DOCUMENT FOREIGN KEY (documentId) REFERENCES Document(id)
+)
+
+create table if not exists DocumentPermission
+(
+	id int GENERATED ALWAYS AS IDENTITY not null,
+	holderId uuid not null, 
+	permission varchar(255) not null,
+
+	CONSTRAINT PK_DOCUMENTPERMISSION PRIMARY KEY(id),
+	CONSTRAINT FK_DOCUMENTPERMISSION_HOLDER FOREIGN KEY (id) REFERENCES Holder(id),
+)
+
+/*
+
+TODO: Define indexes
+TODO: Write stored procedures that will be needed
+
+Implement a simple document storage api
+● User must be able to log in
+● Users can upload and download documents with metadata (posted date, name, description, and category)
+● Users can create groups, and manage users
+
+● Implement groups: a user can belong to one or more groups
+● Document access can be granted to groups or directly to users
+● Implement at least three roles:
+	○ regular user can download documents
+	○ manager user can upload and download documents
+	○ admin can crud users, crud groups, upload documents and download documents
+
+● Make sure to create database tables and stored procedures. You can create .NET object
+to db type mapper
+
+*/
