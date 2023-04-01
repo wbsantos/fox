c74fd22ba732b05aebc724ba375d8ede4baa0b77
William Barbosa <wbs.developer@gmail.com>
2023-04-01 09:39:04 -0300

    Fixing table names and order of creation

--- database/db-setup.sql
+++ database/db-setup.sql
@@ -1,22 +1,34 @@
 --create database fox
+/* 
+// To recreate all tables, they can be dropped using the commands bellow
+drop table DocumentPermission;
+drop table DocumentMetadata;
+drop table Document;
+drop table SystemPermission;
+drop table UserGroup;
+drop table Stamp;
+drop table UserAccount;
+drop table GroupAccount;
+drop table Holder;
+*/
 
 create table if not exists Holder --it's either an user or a group id
 (
 	id uuid default gen_random_uuid() not null,
 
 	CONSTRAINT PK_HOLDER PRIMARY KEY(id)
-)
+);
 
-create table if not exists Group
+create table if not exists GroupAccount
 (
 	id uuid default gen_random_uuid() not null,
 	name varchar(255) not null,
 
-	CONSTRAINT PK_GROUP PRIMARY KEY(id),
-	CONSTRAINT FK_GROUP_HOLDER FOREIGN KEY (id) REFERENCES Holder(id),
-)
+	CONSTRAINT PK_GROUPACCOUNT PRIMARY KEY(id),
+	CONSTRAINT FK_GROUPACCOUNT_HOLDER FOREIGN KEY (id) REFERENCES Holder(id)
+);
 
-create table if not exists User
+create table if not exists UserAccount
 (
 	id uuid default gen_random_uuid() not null,
 	email varchar(255) not null,
@@ -26,9 +38,20 @@ create table if not exists User
 	hashMethod smallint not null,
 	name varchar(255) not null,
 
-	CONSTRAINT PK_USER PRIMARY KEY(id),
-	CONSTRAINT FK_USER_HOLDER FOREIGN KEY (id) REFERENCES Holder(id),
-)
+	CONSTRAINT PK_USERACCOUNT PRIMARY KEY(id),
+	CONSTRAINT FK_USERACCOUNT_HOLDER FOREIGN KEY (id) REFERENCES Holder(id)
+);
+
+create table if not exists Stamp
+(
+	id int GENERATED ALWAYS AS IDENTITY not null,
+	userId uuid,
+	systemVersion varchar(255),
+	createdAt timestamptz,
+
+	CONSTRAINT PK_STAMP PRIMARY KEY(id),
+	CONSTRAINT FK_STAMP_USERACCOUNT FOREIGN KEY (userId) REFERENCES UserAccount(id)
+);
 
 create table if not exists UserGroup
 (
@@ -38,9 +61,9 @@ create table if not exists UserGroup
 	stampId int not null,
 
 	CONSTRAINT PK_USERGROUP PRIMARY KEY(id),
-	CONSTRAINT FK_USERGROUP_USER FOREIGN KEY (userId) REFERENCES User(id),
-	CONSTRAINT FK_USERGROUP_GROUP FOREIGN KEY (groupId) REFERENCES Group(id)
-)
+	CONSTRAINT FK_USERGROUP_USERACCOUNT FOREIGN KEY (userId) REFERENCES UserAccount(id),
+	CONSTRAINT FK_USERGROUP_GROUPACCOUNT FOREIGN KEY (groupId) REFERENCES GroupAccount(id)
+);
 
 create table if not exists SystemPermission
 (
@@ -52,18 +75,7 @@ create table if not exists SystemPermission
 	CONSTRAINT PK_SYSTEMPERMISSION PRIMARY KEY(id),
 	CONSTRAINT FK_SYSTEMPERMISSION_HOLDER FOREIGN KEY (holderId) REFERENCES Holder(id),
 	CONSTRAINT FK_SYSTEMPERMISSION_STAMP FOREIGN KEY (stampId) REFERENCES Stamp(id)	
-)
-
-create table if not exists Stamp
-(
-	id int GENERATED ALWAYS AS IDENTITY not null,
-	userId uuid,
-	systemVersion varchar(255),
-	createdAt timestamptz,
-
-	CONSTRAINT PK_STAMP PRIMARY KEY(id),
-	CONSTRAINT FK_STAMP_USER FOREIGN KEY (userId) REFERENCES User(id)
-)
+);
 
 create table if not exists Document
 (
@@ -75,7 +87,7 @@ create table if not exists Document
 
 	CONSTRAINT PK_DOCUMENT PRIMARY KEY(id),
 	CONSTRAINT FK_DOCUMENT_STAMP FOREIGN KEY (stampId) REFERENCES Stamp(id)	
-)
+);
 
 create table if not exists DocumentMetadata
 (
@@ -86,7 +98,7 @@ create table if not exists DocumentMetadata
 
 	CONSTRAINT PK_DOCUMENTMETADATA PRIMARY KEY(id),
 	CONSTRAINT FK_DOCUMENTMETADATA_DOCUMENT FOREIGN KEY (documentId) REFERENCES Document(id)
-)
+);
 
 create table if not exists DocumentPermission
 (
@@ -103,23 +115,5 @@ create table if not exists DocumentPermission
 )
 
 /*
-
 TODO: Define indexes
-TODO: Write stored procedures regarding System permissions
-
-Implement a simple document storage api
-● User must be able to log in
-● Users can upload and download documents with metadata (posted date, name, description, and category)
-● Users can create groups, and manage users
-
-● Implement groups: a user can belong to one or more groups
-● Document access can be granted to groups or directly to users
-● Implement at least three roles:
-	○ regular user can download documents
-	○ manager user can upload and download documents
-	○ admin can crud users, crud groups, upload documents and download documents
-
-● Make sure to create database tables and stored procedures. You can create .NET object
-to db type mapper
-
 */
