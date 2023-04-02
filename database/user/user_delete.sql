3c445f48a21f089bf44faf8c43c74d3143170978
William Barbosa <wbs.developer@gmail.com>
2023-04-02 12:38:55 -0300

    Fixed procedure that deletes user

--- database/user/user_delete.sql
+++ database/user/user_delete.sql
@@ -6,6 +6,7 @@ BEGIN
 	DELETE FROM SystemPermission WHERE holderId = _id;
 	DELETE FROM UserGroup WHERE userId = _id;
 
+	DELETE FROM Stamp WHERE userId = _id;
 	DELETE FROM UserAccount WHERE id = _id;
 	DELETE FROM Holder WHERE id = _id;
 END
