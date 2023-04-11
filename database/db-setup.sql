421923fd0a2d1ef476cd2c3267d2adbac5cf7606
William Barbosa <wbs.developer@gmail.com>
2023-04-11 08:09:45 -0300

    db-setup.sql script changed to lower case

--- database/db-setup.sql
+++ database/db-setup.sql
@@ -1,22 +1,22 @@
 --create database fox
 /* 
-// To recreate all tables, they can be dropped using the commands bellow
-drop table DocumentPermission;
-drop table DocumentMetadata;
-drop table Document;
-drop table SystemPermission;
-drop table UserGroup;
-drop table Stamp;
-drop table UserAccount;
-drop table GroupAccount;
-drop table Holder;
+// to recreate all tables, they can be dropped using the commands bellow
+drop table document_permission;
+drop table document_metadata;
+drop table document;
+drop table system_permission;
+drop table user_group;
+drop table stamp;
+drop table user_account;
+drop table group_account;
+drop table holder;
 */
 
 create table if not exists holder --it's either an user or a group id
 (
 	id uuid default gen_random_uuid() not null,
 
-	CONSTRAINT PK_HOLDER PRIMARY KEY(id)
+	constraint pk_holder primary key(id)
 );
 
 create table if not exists group_account
@@ -24,8 +24,8 @@ create table if not exists group_account
 	id uuid default gen_random_uuid() not null,
 	name varchar(255) not null,
 
-	CONSTRAINT PK_GROUPACCOUNT PRIMARY KEY(id),
-	CONSTRAINT FK_GROUPACCOUNT_HOLDER FOREIGN KEY (id) REFERENCES holder(id)
+	constraint pk_groupaccount primary key(id),
+	constraint fk_groupaccount_holder foreign key (id) references holder(id)
 );
 
 create table if not exists user_account
@@ -38,44 +38,44 @@ create table if not exists user_account
 	hash_method int not null,
 	name varchar(255) not null,
 
-	CONSTRAINT PK_USERACCOUNT PRIMARY KEY(id),
-	CONSTRAINT FK_USERACCOUNT_HOLDER FOREIGN KEY (id) REFERENCES holder(id)
+	constraint pk_useraccount primary key(id),
+	constraint fk_useraccount_holder foreign key (id) references holder(id)
 );
 
 create table if not exists stamp
 (
-	id int GENERATED ALWAYS AS IDENTITY not null,
+	id int generated always as identity not null,
 	user_id uuid,
 	system_version varchar(255),
 	created_at timestamptz,
 
-	CONSTRAINT PK_STAMP PRIMARY KEY(id),
-	CONSTRAINT FK_STAMP_USERACCOUNT FOREIGN KEY (user_id) REFERENCES user_account(id)
+	constraint pk_stamp primary key(id),
+	constraint fk_stamp_useraccount foreign key (user_id) references user_account(id)
 );
 
 create table if not exists user_group
 (
-	id int GENERATED ALWAYS AS IDENTITY not null,
+	id int generated always as identity not null,
 	user_id uuid not null,
 	group_id uuid not null,
 	stamp_id int not null,
 
-	CONSTRAINT PK_USERGROUP PRIMARY KEY(id),
-	CONSTRAINT FK_USERGROUP_USERACCOUNT FOREIGN KEY (user_id) REFERENCES user_account(id),
-	CONSTRAINT FK_USERGROUP_GROUPACCOUNT FOREIGN KEY (group_id) REFERENCES group_account(id)
-	CONSTRAINT FK_USERGROUP_STAMP FOREIGN KEY (stamp_id) REFERENCES stamp(id)	
+	constraint pk_usergroup primary key(id),
+	constraint fk_usergroup_useraccount foreign key (user_id) references user_account(id),
+	constraint fk_usergroup_groupaccount foreign key (group_id) references group_account(id),
+	constraint fk_usergroup_stamp foreign key (stamp_id) references stamp(id)	
 );
 
 create table if not exists system_permission
 (
-	id int GENERATED ALWAYS AS IDENTITY not null,
+	id int generated always as identity not null,
 	stamp_id int not null,
 	holder_id uuid not null,
 	permission varchar(255) not null,
 
-	CONSTRAINT PK_SYSTEMPERMISSION PRIMARY KEY(id),
-	CONSTRAINT FK_SYSTEMPERMISSION_HOLDER FOREIGN KEY (holder_id) REFERENCES holder(id),
-	CONSTRAINT FK_SYSTEMPERMISSION_STAMP FOREIGN KEY (stamp_id) REFERENCES stamp(id)	
+	constraint pk_systempermission primary key(id),
+	constraint fk_systempermission_holder foreign key (holder_id) references holder(id),
+	constraint fk_systempermission_stamp foreign key (stamp_id) references stamp(id)	
 );
 
 create table if not exists document
@@ -86,45 +86,45 @@ create table if not exists document
 	file_binary bytea not null,
 	file_size_bytes int,
 
-	CONSTRAINT PK_DOCUMENT PRIMARY KEY(id),
-	CONSTRAINT FK_DOCUMENT_STAMP FOREIGN KEY (stamp_id) REFERENCES stamp(id)	
+	constraint pk_document primary key(id),
+	constraint fk_document_stamp foreign key (stamp_id) references stamp(id)	
 );
 
 create table if not exists document_metadata
 (
-	id int GENERATED ALWAYS AS IDENTITY not null,
+	id int generated always as identity not null,
 	document_id uuid not null,
 	key varchar(255) not null,
 	value varchar(1023),
 
-	CONSTRAINT PK_DOCUMENTMETADATA PRIMARY KEY(id),
-	CONSTRAINT FK_DOCUMENTMETADATA_DOCUMENT FOREIGN KEY (document_id) REFERENCES document(id)
+	constraint pk_documentmetadata primary key(id),
+	constraint fk_documentmetadata_document foreign key (document_id) references document(id)
 );
 
 create table if not exists document_permission
 (
-	id int GENERATED ALWAYS AS IDENTITY not null,
+	id int generated always as identity not null,
 	stamp_id int not null,
 	holder_id uuid not null, 
 	document_id uuid not null,
 	permission varchar(255) not null,
 
-	CONSTRAINT PK_DOCUMENTPERMISSION PRIMARY KEY(id),
-	CONSTRAINT FK_DOCUMENTPERMISSION_HOLDER FOREIGN KEY (holder_id) REFERENCES holder(id),
-	CONSTRAINT FK_DOCUMENTPERMISSION_DOCUMENT FOREIGN KEY (document_id) REFERENCES document(id),
-	CONSTRAINT FK_DOCUMENTPERMISSION_STAMP FOREIGN KEY (stamp_id) REFERENCES stamp(id)
+	constraint pk_documentpermission primary key(id),
+	constraint fk_documentpermission_holder foreign key (holder_id) references holder(id),
+	constraint fk_documentpermission_document foreign key (document_id) references document(id),
+	constraint fk_documentpermission_stamp foreign key (stamp_id) references stamp(id)
 );
 
---CREATING INDEXES
-CREATE UNIQUE INDEX UN_USERACCOUNT_01 ON user_account(login);
-CREATE UNIQUE INDEX UN_GROUPACCOUNT_01 ON group_account(name);
-CREATE UNIQUE INDEX UN_USERGROUP_01 ON user_group(groupId, userId);
-CREATE INDEX IN_STAMP_01 ON stamp(user_id);
-CREATE INDEX IN_STAMP_02 ON stamp(created_at);
-CREATE INDEX IN_USERGROUP_01 ON user_group(user_id);
-CREATE INDEX IN_USERGROUP_02 ON user_group(group_id);
-CREATE INDEX IN_SYSTEMPERMISSION_01 ON system_permission(holder_id);
-CREATE INDEX IN_DOCUMENT_01 ON document(name);
-CREATE INDEX IN_DOCUMENTMETADATA_01 ON document_metadata(key);
-CREATE INDEX IN_DOCUMENTPERMISSION_01 ON document_permission(holder_id);
-CREATE INDEX IN_DOCUMENTPERMISSION_02 ON document_permission(document_id);
+--creating indexes
+create unique index un_useraccount_01 on user_account(login);
+create unique index un_groupaccount_01 on group_account(name);
+create unique index un_usergroup_01 on user_group(group_id, user_id);
+create index in_stamp_01 on stamp(user_id);
+create index in_stamp_02 on stamp(created_at);
+create index in_usergroup_01 on user_group(user_id);
+create index in_usergroup_02 on user_group(group_id);
+create index in_systempermission_01 on system_permission(holder_id);
+create index in_document_01 on document(name);
+create index in_documentmetadata_01 on document_metadata(key);
+create index in_documentpermission_01 on document_permission(holder_id);
+create index in_documentpermission_02 on document_permission(document_id);
